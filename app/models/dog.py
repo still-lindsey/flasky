a1fa5e6a58dcdda245709f3abc554f3f8cd1f70c
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-25 15:03:02 -0700

    Revert "Add default breed and refactor post route"
    This reverts commit 33723533a5371c32d20db904c20dbf37abc89c80.

--- app/models/dog.py
+++ app/models/dog.py
@@ -3,7 +3,7 @@ from app import db
 class Dog(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    breed = db.Column(db.String, default="golden doodle")
+    breed = db.Column(db.String)
 
     def to_dict(self):
         return {
