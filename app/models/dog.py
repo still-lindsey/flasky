673f3c9f886ce3bfbc3ac188aa11c2c001664f07
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-25 11:12:18 -0700

    Add dog model

---
+++ app/models/dog.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Dog(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    breed = db.Column(db.String)
+
+    def to_json(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "breed": self.breed
+            }
