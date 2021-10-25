243613fefc6dc2f597d633d93dcd9df0802e655c
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-25 13:12:18 -0700

    Get and post actions for Dog model

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,32 @@
 from flask import Flask
+# Step 1
+# Import & Initialize SQL Alchemy
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+# Initialize SQL Alchemy
+db = SQLAlchemy()
+migrate = Migrate()
+
+DATABASE_CONNECTION_STRING = \
+    'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
 
 def create_app():
     # __name__ store the name of the module we're in
     app = Flask(__name__)
+
+    # Step 2:
+    # Configure SQLAlchemy
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_CONNECTION_STRING
+
+    # Import Models here!
+    from app.models.dog import Dog
+
+    # Step 3:
+    # Hook up Flask & Sql Alchemy
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes.dog_routes import dog_bp
     app.register_blueprint(dog_bp)
--- app/routes/dog_routes.py
+++ app/routes/dog_routes.py
@@ -1,65 +1,59 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+# Import Model & db
+from app.models.dog import Dog
+from app import db
 
 dog_bp = Blueprint("dog", __name__,url_prefix="/dogs")
 
-class Dog:
-    def __init__(self, id, name, breed, tricks=None):
-        self.id = id
-        self.name = name
-        self.breed = breed
-        if not tricks:
-            tricks = []
-        self.tricks = tricks
+# Make a dog
+@dog_bp.route("", methods=["POST"])
+def create_dog():
+    # Get the data from the request
+    request_data = request.get_json()
 
-    def to_json(self):
-        if not self.tricks:
-            tricks = "No Tricks"
-        else:
-            tricks = self.tricks
+    if "name" not in request_data or "age" not in request_data \
+        or "breed" not in request_data:
+        return jsonify({"message": "Missing data"}), 400
 
-        return {
-                "id": self.id,
-                "name": self.name,
-                "breed": self.breed,
-                "tricks": tricks
-            }
+    new_dog = Dog(name=request_data["name"], age=request_data["age"], 
+                breed=request_data["breed"])
+    
+    db.session.add(new_dog)
+    db.session.commit()
 
-dogs = [
-    Dog(1, "mac", "greyhound"),
-    Dog(2, "sparky", "schnauzer"),
-    Dog(3, "teddy", "golden retriever", ["dance"])
-]
+    return f"Dog {new_dog.name} created", 201
+
+
 
 
 # get all dogs
 @dog_bp.route("", methods=["GET"])
 def handle_dogs():
     dogs_response = []
+    dogs = Dog.query.all()
     for dog in dogs:
-        dogs_response.append(
-            #vars(dog)
-            # {
-            #     "id": dog.id,
-            #     "name": dog.name,
-            #     "breed": dog.breed,
-            #     "tricks": dog.tricks
-            # }
-            dog.to_json()
-        )
-    return jsonify(dogs_response)
+        dogs_response.append({
+            "id": dog.id,
+            "name": dog.name,
+            "age": dog.age,
+            "breed": dog.breed,
+        })
+    
+    return jsonify(dogs_response), 200
+    
 
 @dog_bp.route("/<dog_id>", methods=["GET"])
 def handle_dog(dog_id):
     print(dog_id)
     dog_id = int(dog_id)
-    for dog in dogs:
-        if dog.id == dog_id:
-            # return {
-            #     "id": dog.id,
-            #     "name": dog.name,
-            #     "breed": dog.breed,
-            #     "tricks": dog.tricks
-            # }
-            return dog.to_json()
+    dog = Dog.query.get(dog_id)
 
-    return {"error": "Dog not found"}, 404
+    if dog:
+        return {
+            "id": dog.id,
+            "name": dog.name,
+            "age": dog.age,
+            "breed": dog.breed,
+        }, 200
+    
+    return { "Error": f"Dog {dog_id} was not found"}, 404
