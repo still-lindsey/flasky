2042869b842c6a97218988e1f2215fe07aef40db
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-25 11:01:31 -0700

    Add connection to flasky_development databse

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+DATABASE = 'postgresql+psycopg2://postgres:postgres@localhost:5432/flasky_development'
+
 
 def create_app():
     # __name__ store the name of the module we're in
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes.dog_routes import dog_bp
     app.register_blueprint(dog_bp)
