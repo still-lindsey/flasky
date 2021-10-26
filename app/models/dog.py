b62a280077e6e18b2a49ab4fe79f64ec2373d153
Becca Elenzil <becca@adadevelopersacademy.org>
2021-10-26 12:45:32 -0700

    Add age and query params

--- app/models/dog.py
+++ app/models/dog.py
@@ -4,10 +4,12 @@ class Dog(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
     breed = db.Column(db.String)
+    age = db.Column(db.Integer)
 
     def to_dict(self):
         return {
                 "id": self.id,
                 "name": self.name,
-                "breed": self.breed
+                "breed": self.breed,
+                "age": self.age
             }
--- app/routes/dog_routes.py
+++ app/routes/dog_routes.py
@@ -4,39 +4,61 @@ from flask import Blueprint, jsonify, make_response, request, abort
 
 dog_bp = Blueprint("dog", __name__,url_prefix="/dogs")
 
-@dog_bp.route("", methods=["GET", "POST"])
-def handle_dogs():
-    if request.method == 'POST':
-        request_body = request.get_json()
-        if "name" not in request_body or "breed" not in request_body:
-            return {"error": "incomplete request body"}, 400
-
-
-        new_dog = Dog(
-            name=request_body["name"],
-            breed=request_body["breed"],
-        )
-
-        db.session.add(new_dog)
-        db.session.commit()
-
-        return make_response(f"Dog {new_dog.name} created!", 201)
-    elif request.method == 'GET':
-        dogs = Dog.query.all()
-        dogs_response = []
-        for dog in dogs:
-            dogs_response.append(
-                dog.to_dict()
-            )
-        return jsonify(dogs_response)
+# Helper Functions
+def check_valid_int(number, parameter_type):
+    try:
+        number = int(number)
+    except:
+        abort(make_response({"error": f"{parameter_type} must be an int"}, 400))
 
 def get_dog_from_id(dog_id):
-    try:
-        dog_id = int(dog_id)
-    except:
-        abort(make_response({"error": "dog_id must be an int"}, 400))
-
+    check_valid_int(dog_id, "dog_id")
     return Dog.query.get_or_404(dog_id, description="{dog not found}")
+
+# Routes
+@dog_bp.route("", methods=["GET"])
+def read_all_dogs():
+    
+    age_query = request.args.get("age")
+    older_query = request.args.get("older")
+    sort_query = request.args.get("sort")
+
+    dogs = Dog.query
+
+    if age_query:
+        check_valid_int(age_query, "age")
+        dogs = dogs.filter_by(age=age_query)
+    elif older_query:
+        check_valid_int(older_query, "older")
+        dogs = dogs.filter(Dog.age > older_query)
+
+    if sort_query == "asc":
+        dogs = dogs.order_by(Dog.age.asc())
+    elif sort_query == "desc":
+        dogs = dogs.order_by(Dog.age.desc())
+
+    dogs_response = []
+    for dog in dogs:
+        dogs_response.append(
+            dog.to_dict()
+        )
+    return jsonify(dogs_response)
+
+@dog_bp.route("", methods=["POST"])
+def create_dog():
+    request_body = request.get_json()
+    if "name" not in request_body or "breed" not in request_body:
+        return {"error": "incomplete request body"}, 400
+
+    new_dog = Dog(
+        name=request_body["name"],
+        breed=request_body["breed"],
+    )
+
+    db.session.add(new_dog)
+    db.session.commit()
+
+    return make_response(f"Dog {new_dog.name} created!", 201)
 
 @dog_bp.route("/<dog_id>", methods=["GET"])
 def read_dog(dog_id):
@@ -53,6 +75,8 @@ def update_dog(dog_id):
         dog.name = request_body["name"]
     if "breed" in request_body:
         dog.breed = request_body["breed"]
+    if "age" in request_body:
+        dog.age = request_body["age"]
     
     db.session.commit()
     return jsonify(dog.to_dict())
