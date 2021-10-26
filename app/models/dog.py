0ee1eebfeaa3bf4f62b08d9bc14568a856952c9e
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-26 11:00:11 -0700

    add blank line

--- app/models/dog.py
+++ app/models/dog.py
@@ -2,9 +2,11 @@ from app import db
 
 class Dog(db.Model):
     __tablename__ = 'dogs'
+    
     breed = db.Column(db.String(64))
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     age = db.Column(db.Integer)
     name = db.Column(db.String(64))
+
 
 
