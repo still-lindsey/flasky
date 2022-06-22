718c5c5a0a48f63d697fd069817ec72c0c4c6121
Lindsey Still <lindseymstill@gmail.com>
2022-06-23 02:42:38 +0900

    added cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -33,4 +34,5 @@ def create_app(test_config = None):
     from .routes.caretaker_routes import caretaker_bp
     app.register_blueprint(caretaker_bp)
 
+    CORS(app)
     return app
